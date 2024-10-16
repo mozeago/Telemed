adc48b6fbcce3debf4e12757318d7c34d696dcd0
Moses Asiago <mozzeago@gmail.com>
2024-10-17 01:28:19 +0300

    connection pooling

--- database/db.js
+++ database/db.js
@@ -1,17 +1,13 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const log = require('../utils/logger');
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
-connection.connect((error) => {
-    if (error) {
-        log(`Error Connecting to Database: ${error.message}`);
-        return;
-    }
-    log('Connected to DB!');
-});
-module.exports = connection;
+module.exports = pool;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
-const connection = require('../database/db');
+const pool = require('../database/db');
 const express = require('express');
 const router = express.Router();
 const log = require('../utils/logger');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  connection.query('SELECT * FROM patients', (error, results) => {
-    if (error) {
-      log(`Database query failed: ${error.message}`);
-      return res.status(500).send('A server error occurred while processing your request. Please try again later.');
-    }
-    res.render('index', { title: 'Telemed', patients: results });
-  })
-});
+router.get('/', async(req, re));
 
 module.exports = router;
