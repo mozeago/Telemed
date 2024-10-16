802eb2e01f7abbc62e2242f3c393d9dfb62c6021
Moses Asiago <mozzeago@gmail.com>
2024-10-16 21:32:38 +0300

    added module-alias for easy routes imports

---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+router.post('/register', (request, response) => {
+
+});
+router.post('/login', (request, response) => {
+
+});
+router.post('/logout', (request, response){
+
+});
+module.exports = router
