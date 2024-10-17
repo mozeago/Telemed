b438e49a2337c60ecf19a234ca0af0c9a75787d2
Moses Asiago <mozzeago@gmail.com>
2024-10-17 17:13:22 +0300

    register:complete with validation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,16 +6,44 @@ const pool = require('../database/db');
 const { log, logError } = require('../utils/logger');
 const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 const PNF = require('google-libphonenumber').PhoneNumberFormat;
+const { check, validationResult } = require('express-validator');
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
-router.post('/register', async (req, res) => {
+router.post('/register', [
+    check('first_name').notEmpty().withMessage('First name is required'),
+    check('last_name').notEmpty().withMessage('Last name is required'),
+    check('email').isEmail().withMessage('Please enter a valid email address'),
+    check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    check('phone').notEmpty().withMessage('Phone number is required'),
+    check('date_of_birth').notEmpty().withMessage('Date of birth is required'),
+    check('gender').notEmpty().withMessage('Gender is required'),
+    check('street').notEmpty().withMessage('Street address is required'),
+    check('city').notEmpty().withMessage('City is required'),
+    check('state').notEmpty().withMessage('State is required'),
+    check('postal_code').notEmpty().withMessage('Postal code is required'),
+    check('country').notEmpty().withMessage('Country is required')
+], async (req, res) => {
     const { first_name, last_name, email, password, phone, date_of_birth, gender, street, city, state, postal_code, country } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).render('auth/register', {
+            errors: errors.array(),
+            first_name,
+            last_name,
+            email,
+            phone,
+            date_of_birth,
+            gender,
+            street,
+            city,
+            state,
+            postal_code,
+            country
+        })
+    }
     try {
         const number = phoneUtil.parse(phone);
-        // if (!phoneUtil.isValidNumber(number)) {
-        //     return res.status(400).send('Invalid phone number format');
-        // }
         const formattedPhone = phoneUtil.format(number, PNF.E164);
         const queryStatement = 'INSERT INTO patients (first_name, last_name, email, password_hash, phone, date_of_birth, gender,address) VALUES (?, ?, ?, ?, ?, ?, ?,?)';
         const hashedPassword = await bcrypt.hash(password, 10);
